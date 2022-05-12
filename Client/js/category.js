1e1dfa0c136ea99045c7aa01abd603c23ecfc0d0
WRBKOR23 <pthai.it.210501@gmail.com>
2022-05-12 23:09:31 +0700

    fdasfdfasd

---
+++ Client/js/category.js
@@ -0,0 +1,37 @@
+let ulTag = document.getElementsByClassName('hero__categories')[0].childNodes[3];
+ulTag.innerHTML = ''
+
+async function getCategories() {
+    const init = {
+        method: 'GET',
+        cache: 'no-cache',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+        },
+    };
+    const url = 'https://localhost/agricultural-products-store/public/api/v1/categories';
+
+    let response = await fetch(url, init);
+    if (response.status === 200) {
+        let responseJson = await response.json();
+        let data = await responseJson.data;
+        createCategories(data);
+    }
+}
+
+function createCategories(data) {
+    console.log(data);
+    data.forEach(element => {
+        let li = document.createElement('li');
+        let a = document.createElement('a');
+        a.href = '#';
+        a.innerHTML = element.name;
+        a.id = element.id;
+        li.appendChild(a);
+        ulTag.appendChild(li);
+    });
+
+}
+
+getCategories();
